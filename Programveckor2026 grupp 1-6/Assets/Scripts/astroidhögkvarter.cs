d471807790fac26472d83ebaad8c40224fca62a5
Villehanville <vilhelm.engstrom@elev.ga.lbs.se>
2026-01-12 10:20:19 +0100

    ololo

---
+++ Programveckor2026 grupp 1-6/Assets/Scripts/astroidhögkvarter.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class astroidhögkvarter : MonoBehaviour
+{
+    public GameObject al;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int placering = gameObject.GetComponent<wormholePlayerTP>().rumsplacering;
+        if (placering == 1)
+        {
+            gameObject.CompareTag("a1")
+        }
+        if (placering == 2)
+        {
+
+        }
+        if (placering == 3)
+        {
+
+        }
+        if (placering == 4)
+        {
+
+        }
+        if (placering == 5)
+        {
+
+        }
+        if (placering == 6)
+        {
+
+        }
+        if (placering == 7)
+        {
+
+        }
+    }
+}

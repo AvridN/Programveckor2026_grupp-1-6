69fdc914640b7cb4c96a85b5a6461c57e3a51c8c
Villehanville <vilhelm.engstrom@elev.ga.lbs.se>
2026-01-12 09:36:06 +0100

    player wormholescript

--- Programveckor2026 grupp 1-6/Assets/Scripts/wormholePlayerTP.cs
+++ Programveckor2026 grupp 1-6/Assets/Scripts/wormholePlayerTP.cs
@@ -26,6 +26,25 @@ public class wormholePlayerTP : MonoBehaviour
         if (collision.gameObject.CompareTag("Övredörr") && Input.GetKeyDown(KeyCode.E))
         {
             int rumplacering = Random.Range(1, 7);
+            transform.position = new Vector2(0.09f, -4.69f);
+
+        }
+        if (collision.gameObject.CompareTag("Högradörr") && Input.GetKeyDown(KeyCode.E))
+        {
+            int rumsplacering = Random.Range(1, 7);
+            transform.position = new Vector2(-8.33f, -0.02f);
+
+        }
+        if (collision.gameObject.CompareTag("Vänstradörr") && Input.GetKeyDown(KeyCode.E))
+        {
+            int rumsplacering = Random.Range(1, 7);
+            transform.position = new Vector2(8.56f, -0.19f);
+
+        }
+        if (collision.gameObject.CompareTag("Nedredörr") && Input.GetKeyDown(KeyCode.E))
+        {
+            int rumsplacering = Random.Range(1, 7);
+            transform.position = new Vector2(0.22f, 4.5f);
 
         }
     }

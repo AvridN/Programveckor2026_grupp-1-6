21a0497c76ed9936d0dc9225bb0df242f39f5e98
Ugglmannen <simon.stamreus@elev.ga.lbs.se>
2026-01-12 09:33:55 +0100

    hurt player tillagt
    nu har spelaren ett hp och kommer ladda om sean 1 efter 3 hits

--- Programveckor2026 grupp 1-6/Assets/scripts/hurtplayer.cs
+++ Programveckor2026 grupp 1-6/Assets/scripts/hurtplayer.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class hurtplayer : MonoBehaviour
 {
+    public object collision;
+    int hurtamount = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +18,17 @@ public class hurtplayer : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("PlayerMovement")) { }
+
+        if (collision.gameObject.CompareTag("enemy_movment"))  // ska ändras till fiender script
+        {
+            hurtamount++;
+
+
+        }
+        if (hurtamount == 3)
+        {
+            SceneManager.LoadScene(1);
+
+        }
     }
 }

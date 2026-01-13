e349403367d2261ae212e0a6c7af93d20dcc18c6
Ugglmannen <simon.stamreus@elev.ga.lbs.se>
2026-01-13 09:09:47 +0100

    01 13
    simon pls fungera

---
+++ Programveckor2026 grupp 1-6/Assets/renamn scripts/enemy_life_and_kill.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class hurt_enemy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Hoock"))  // ska ändras till fiender script
+        {
+            Destroy(gameObject);
+
+
+        }
+
+
+
+
+    }
+}

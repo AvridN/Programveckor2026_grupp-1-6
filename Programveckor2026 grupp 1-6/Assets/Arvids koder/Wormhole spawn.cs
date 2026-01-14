bf55dd7bf28a5e93148d98dc09afd86818557899
AvridN <arvid.nyback@elev.ga.lbs.se>
2026-01-14 10:54:01 +0100

    demon
    demon

--- Programveckor2026 grupp 1-6/Assets/Arvids koder/Wormhole spawn.cs
+++ Programveckor2026 grupp 1-6/Assets/Arvids koder/Wormhole spawn.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Wormholespawn : MonoBehaviour
 {
+    GameObject[] enemies;
+    public GameObject Wormhole;
 
+    bool baraengång = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +15,17 @@ public class Wormholespawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        enemies = GameObject.FindGameObjectsWithTag("enemy_movment");
+        if (enemies.Length == 0)
+        {
+            if (baraengång == true)
+            {
 
+
+                Vector2 spawnlocation = new Vector2(-7f, 0f);
+                Instantiate(Wormhole, spawnlocation, Quaternion.identity);
+                baraengång = false;
+            }
+        }
     }
 }

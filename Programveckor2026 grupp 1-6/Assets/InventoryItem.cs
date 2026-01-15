d1050d964fc8ce338e46cac6162981f169fa7a71
MasterYoda-896BBY <edvin.ohrstromjuhlen@elev.ga.lbs.se>
2026-01-15 09:12:18 +0100

    tp till shop och tillbaka
    press e to tp

---
+++ Programveckor2026 grupp 1-6/Assets/InventoryItem.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class InventoryItem : ScriptableObject
+{
+    public Sprite itemImage;
+    public string itemName;
+    public int cost;
+    public int strength;
+}
